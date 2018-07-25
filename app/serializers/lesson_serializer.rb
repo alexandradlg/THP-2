08488ff9800ee116d03658977ae0b022a45badd7
Alexandra DLG <adelagueronniere@gmail.com>
2018-07-25 11:05:10 +0200

    add guard

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id          :uuid             not null, primary key
+#  title       :string(50)
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class LessonSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :created_at
 end
